cfbae09a16552ec798e4ba5bf8589baab6dd7700
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2023-07-01 20:26:31 +0800

    Update research.py
    Signed-off-by: theMoon321 <130368802+theMoon321@users.noreply.github.com>

--- models/Models/research.py
+++ models/Models/research.py
@@ -18,6 +18,20 @@ from PIL import Image
 from torch.cuda import amp
 
 # ---------------------------------------------------------------------------
+class Conv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super(Conv, self).__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
+
 class MHSA(nn.Module):
     def __init__(self, n_dims, width=14, height=14, heads=4, pos_emb=False):
         super(MHSA, self).__init__()
