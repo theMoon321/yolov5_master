13f4196f413be1b238e62f76d2a6b05562541747
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2023-07-02 09:38:14 +0800

    Update research.py
    Signed-off-by: theMoon321 <130368802+theMoon321@users.noreply.github.com>

--- models/Models/research.py
+++ models/Models/research.py
@@ -18,6 +18,12 @@ from PIL import Image
 from torch.cuda import amp
 
 # ---------------------------------------------------------------------------
+def autopad(k, p=None):  # kernel, padding
+    # Pad to 'same'
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
 class Conv(nn.Module):
     # Standard convolution
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
