b5abaf76067de2681e308a8256c023ecd9dee321
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2024-01-17 14:58:30 +0800

    Update research.py
    Signed-off-by: theMoon321 <130368802+theMoon321@users.noreply.github.com>

--- models/Models/research.py
+++ models/Models/research.py
@@ -25,7 +25,7 @@ from torch.cuda import amp
 
 from utils.datasets import exif_transpose, letterbox
 from utils.general import (LOGGER, check_requirements, check_suffix, check_version, colorstr, increment_path,
-                           make_divisible, non_max_suppression, scale_coords, xywh2xyxy, xyxy2xywh)
+                           make_divisible, non_max_suppression xywh2xyxy, xyxy2xywh)
 from utils.plots import Annotator, colors,save_one_box
 from utils.torch_utils import copy_attr, time_sync
 
@@ -912,100 +912,100 @@ class ShuffleNetV2(nn.Module):
 
 
 
-class AutoShape(nn.Module):
-    # input-robust model wrapper for passing cv2/np/PIL/torch inputs. Includes preprocessing, inference and NMS
-    conf = 0.25  # NMS confidence threshold
-    iou = 0.45  # NMS IoU threshold
-    agnostic = False  # NMS class-agnostic
-    multi_label = False  # NMS multiple labels per box
-    classes = None  # (optional list) filter by class, i.e. = [0, 15, 16] for COCO persons, cats and dogs
-    max_det = 1000  # maximum number of detections per image
-    amp = False  # Automatic Mixed Precision (AMP) inference
+# class AutoShape(nn.Module):
+#     # input-robust model wrapper for passing cv2/np/PIL/torch inputs. Includes preprocessing, inference and NMS
+#     conf = 0.25  # NMS confidence threshold
+#     iou = 0.45  # NMS IoU threshold
+#     agnostic = False  # NMS class-agnostic
+#     multi_label = False  # NMS multiple labels per box
+#     classes = None  # (optional list) filter by class, i.e. = [0, 15, 16] for COCO persons, cats and dogs
+#     max_det = 1000  # maximum number of detections per image
+#     amp = False  # Automatic Mixed Precision (AMP) inference
     
-    def __init__(self, model, verbose=True):
-        super().__init__()
-        if verbose:
-            LOGGER.info('Adding AutoShape... ')
-        copy_attr(self, model, include=('yaml', 'nc', 'hyp', 'names', 'stride', 'abc'), exclude=())  # copy attributes
-        self.dmb = isinstance(model, DetectMultiBackend)  # DetectMultiBackend() instance
-        self.pt = not self.dmb or model.pt  # PyTorch model
-        self.model = model.eval()
-        if self.pt:
-            m = self.model.model.model[-1] if self.dmb else self.model.model[-1]  # Detect()
-            m.inplace = False  # Detect.inplace=False for safe multithread inference
+#     def __init__(self, model, verbose=True):
+#         super().__init__()
+#         if verbose:
+#             LOGGER.info('Adding AutoShape... ')
+#         copy_attr(self, model, include=('yaml', 'nc', 'hyp', 'names', 'stride', 'abc'), exclude=())  # copy attributes
+#         self.dmb = isinstance(model, DetectMultiBackend)  # DetectMultiBackend() instance
+#         self.pt = not self.dmb or model.pt  # PyTorch model
+#         self.model = model.eval()
+#         if self.pt:
+#             m = self.model.model.model[-1] if self.dmb else self.model.model[-1]  # Detect()
+#             m.inplace = False  # Detect.inplace=False for safe multithread inference
 
-    def _apply(self, fn):
-            # Apply to(), cpu(), cuda(), half() to model tensors that are not parameters or registered buffers
-            self = super()._apply(fn)
-            if self.pt:
-                m = self.model.model.model[-1] if self.dmb else self.model.model[-1]  # Detect()
-                m.stride = fn(m.stride)
-                m.grid = list(map(fn, m.grid))
-                if isinstance(m.anchor_grid, list):
-                    m.anchor_grid = list(map(fn, m.anchor_grid))
-            return self
+#     def _apply(self, fn):
+#             # Apply to(), cpu(), cuda(), half() to model tensors that are not parameters or registered buffers
+#             self = super()._apply(fn)
+#             if self.pt:
+#                 m = self.model.model.model[-1] if self.dmb else self.model.model[-1]  # Detect()
+#                 m.stride = fn(m.stride)
+#                 m.grid = list(map(fn, m.grid))
+#                 if isinstance(m.anchor_grid, list):
+#                     m.anchor_grid = list(map(fn, m.anchor_grid))
+#             return self
 
-    @torch.no_grad()
-    def forward(self, imgs, size=640, augment=False, profile=False):
-        # Inference from various sources. For height=640, width=1280, RGB images example inputs are:
-        #   filename:   imgs = 'data/images/zidane.jpg'
-        #   URI:             = 'https://github.com/ultralytics/yolov5/releases/download/v1.0/zidane.jpg'
-        #   OpenCV:          = cv2.imread('image.jpg')[:,:,::-1]  # HWC BGR to RGB x(640,1280,3)
-        #   PIL:             = Image.open('image.jpg')  # HWC x(640,1280,3)
-        #   numpy:           = np.zeros((640,1280,3))  # HWC
-        #   torch:           = torch.zeros(16,3,320,640)  # BCHW (scaled to size=640, 0-1 values)
-        #   multiple:        = [Image.open('image1.jpg'), Image.open('image2.jpg'), ...]  # list of images
+#     @torch.no_grad()
+#     def forward(self, imgs, size=640, augment=False, profile=False):
+#         # Inference from various sources. For height=640, width=1280, RGB images example inputs are:
+#         #   filename:   imgs = 'data/images/zidane.jpg'
+#         #   URI:             = 'https://github.com/ultralytics/yolov5/releases/download/v1.0/zidane.jpg'
+#         #   OpenCV:          = cv2.imread('image.jpg')[:,:,::-1]  # HWC BGR to RGB x(640,1280,3)
+#         #   PIL:             = Image.open('image.jpg')  # HWC x(640,1280,3)
+#         #   numpy:           = np.zeros((640,1280,3))  # HWC
+#         #   torch:           = torch.zeros(16,3,320,640)  # BCHW (scaled to size=640, 0-1 values)
+#         #   multiple:        = [Image.open('image1.jpg'), Image.open('image2.jpg'), ...]  # list of images
 
-        t = [time_sync()]
-        p = next(self.model.parameters()) if self.pt else torch.zeros(1, device=self.model.device)  # for device and type
-        autocast = self.amp and (p.device.type != 'cpu')  # Automatic Mixed Precision (AMP) inference
-        if isinstance(imgs, torch.Tensor):  # torch
-            with amp.autocast(autocast):
-                return self.model(imgs.to(p.device).type_as(p), augment, profile)  # inference
+#         t = [time_sync()]
+#         p = next(self.model.parameters()) if self.pt else torch.zeros(1, device=self.model.device)  # for device and type
+#         autocast = self.amp and (p.device.type != 'cpu')  # Automatic Mixed Precision (AMP) inference
+#         if isinstance(imgs, torch.Tensor):  # torch
+#             with amp.autocast(autocast):
+#                 return self.model(imgs.to(p.device).type_as(p), augment, profile)  # inference
 
-        # Pre-process
-        n, imgs = (len(imgs), imgs) if isinstance(imgs, list) else (1, [imgs])  # number of images, list of images
-        shape0, shape1, files = [], [], []  # image and inference shapes, filenames
-        for i, im in enumerate(imgs):
-            f = f'image{i}'  # filename
-            if isinstance(im, (str, Path)):  # filename or uri
-                im, f = Image.open(requests.get(im, stream=True).raw if str(im).startswith('http') else im), im
-                im = np.asarray(exif_transpose(im))
-            elif isinstance(im, Image.Image):  # PIL Image
-                im, f = np.asarray(exif_transpose(im)), getattr(im, 'filename', f) or f
-            files.append(Path(f).with_suffix('.jpg').name)
-            if im.shape[0] < 5:  # image in CHW
-                im = im.transpose((1, 2, 0))  # reverse dataloader .transpose(2, 0, 1)
-            im = im[..., :3] if im.ndim == 3 else np.tile(im[..., None], 3)  # enforce 3ch input
-            s = im.shape[:2]  # HWC
-            shape0.append(s)  # image shape
-            g = (size / max(s))  # gain
-            shape1.append([y * g for y in s])
-            imgs[i] = im if im.data.contiguous else np.ascontiguousarray(im)  # update
-        shape1 = [make_divisible(x, self.stride) for x in np.stack(shape1, 0).max(0)]  # inference shape
-        x = [letterbox(im, shape1, auto=False)[0] for im in imgs]  # pad
-        x = np.ascontiguousarray(np.array(x).transpose((0, 3, 1, 2)))  # stack and BHWC to BCHW
-        x = torch.from_numpy(x).to(p.device).type_as(p) / 255.  # uint8 to fp16/32
-        t.append(time_sync())
+#         # Pre-process
+#         n, imgs = (len(imgs), imgs) if isinstance(imgs, list) else (1, [imgs])  # number of images, list of images
+#         shape0, shape1, files = [], [], []  # image and inference shapes, filenames
+#         for i, im in enumerate(imgs):
+#             f = f'image{i}'  # filename
+#             if isinstance(im, (str, Path)):  # filename or uri
+#                 im, f = Image.open(requests.get(im, stream=True).raw if str(im).startswith('http') else im), im
+#                 im = np.asarray(exif_transpose(im))
+#             elif isinstance(im, Image.Image):  # PIL Image
+#                 im, f = np.asarray(exif_transpose(im)), getattr(im, 'filename', f) or f
+#             files.append(Path(f).with_suffix('.jpg').name)
+#             if im.shape[0] < 5:  # image in CHW
+#                 im = im.transpose((1, 2, 0))  # reverse dataloader .transpose(2, 0, 1)
+#             im = im[..., :3] if im.ndim == 3 else np.tile(im[..., None], 3)  # enforce 3ch input
+#             s = im.shape[:2]  # HWC
+#             shape0.append(s)  # image shape
+#             g = (size / max(s))  # gain
+#             shape1.append([y * g for y in s])
+#             imgs[i] = im if im.data.contiguous else np.ascontiguousarray(im)  # update
+#         shape1 = [make_divisible(x, self.stride) for x in np.stack(shape1, 0).max(0)]  # inference shape
+#         x = [letterbox(im, shape1, auto=False)[0] for im in imgs]  # pad
+#         x = np.ascontiguousarray(np.array(x).transpose((0, 3, 1, 2)))  # stack and BHWC to BCHW
+#         x = torch.from_numpy(x).to(p.device).type_as(p) / 255.  # uint8 to fp16/32
+#         t.append(time_sync())
 
-        with amp.autocast(autocast):
-            # Inference
-            y = self.model(x, augment, profile)  # forward
-            t.append(time_sync())
+#         with amp.autocast(autocast):
+#             # Inference
+#             y = self.model(x, augment, profile)  # forward
+#             t.append(time_sync())
 
-            # Post-process
-            y = non_max_suppression(y if self.dmb else y[0],
-                                    self.conf,
-                                    self.iou,
-                                    self.classes,
-                                    self.agnostic,
-                                    self.multi_label,
-                                    max_det=self.max_det)  # NMS
-            for i in range(n):
-                scale_coords(shape1, y[i][:, :4], shape0[i])
+#             # Post-process
+#             y = non_max_suppression(y if self.dmb else y[0],
+#                                     self.conf,
+#                                     self.iou,
+#                                     self.classes,
+#                                     self.agnostic,
+#                                     self.multi_label,
+#                                     max_det=self.max_det)  # NMS
+#             for i in range(n):
+#                 scale_coords(shape1, y[i][:, :4], shape0[i])
 
-            t.append(time_sync())
-            return Detections(imgs, y, files, t, self.names, x.shape)
+#             t.append(time_sync())
+#             return Detections(imgs, y, files, t, self.names, x.shape)
 
 
 class Detections:
