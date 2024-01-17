e626eaf39ff37cbde1f6cee1a28a35debab2e141
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2024-01-17 15:10:01 +0800

    Update yolo.py
    Signed-off-by: theMoon321 <130368802+theMoon321@users.noreply.github.com>

--- models/yolo.py
+++ models/yolo.py
@@ -337,7 +337,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         # YSY: add module research begin-----------------------------
         elif m in [CARAFE, SPPCSPC, SPPFCSPC, RepConv, BoT3, CA, CBAM, Involution, Stem, ResCSPC, ResCSPB, \
                    ResXCSPB, ResXCSPC, BottleneckCSPB, BottleneckCSPC,
-                   ASPP, BasicRFB, SPPCSPC_group, HorBlock, CNeB,C3GC ,C3C2, nn.ConvTranspose2d]:
+                   ASPP, BasicRFB, SPPCSPC_group, HorBlock, C3GC ,C3C2, nn.ConvTranspose2d]:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
