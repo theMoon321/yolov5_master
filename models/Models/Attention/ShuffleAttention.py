d69a9f342231c4ae31d8db1762a808bb756b1142
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2024-01-17 14:38:43 +0800

    Update ShuffleAttention.py
    Signed-off-by: theMoon321 <130368802+theMoon321@users.noreply.github.com>

--- models/Models/Attention/ShuffleAttention.py
+++ models/Models/Attention/ShuffleAttention.py
@@ -1,128 +1,73 @@
-# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
-"""
-    GSConv is used to merge the channel information of DSConv and BaseConv
-    You can refer to https://github.com/AlanLi1997/slim-neck-by-gsconv for more details
-"""
-
-import json
-import math
-import platform
-import warnings
-from collections import OrderedDict, namedtuple
-from copy import copy
-from pathlib import Path
-
-import cv2
 import numpy as np
-import pandas as pd
-import requests
 import torch
-import torch.nn as nn
-import yaml
-from PIL import Image
-from torch.cuda import amp
+from torch import nn
+from torch.nn import init
+from torch.nn.parameter import Parameter
 
-def autopad(k, p=None):  # kernel, padding
-    # Pad to 'same'
-    if p is None:
-        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
-    return p
+# https://arxiv.org/pdf/2102.00240.pdf
+class ShuffleAttention(nn.Module):
 
-class Conv(nn.Module):
-    # Standard convolution
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+    def __init__(self, channel=512,reduction=16,G=8):
         super().__init__()
-        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
-        self.bn = nn.BatchNorm2d(c2)
-        self.act = nn.Mish() if act else nn.Identity()
+        self.G=G
+        self.channel=channel
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.gn = nn.GroupNorm(channel // (2 * G), channel // (2 * G))
+        self.cweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
+        self.cbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
+        self.sweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
+        self.sbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
+        self.sigmoid=nn.Sigmoid()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+
+    @staticmethod
+    def channel_shuffle(x, groups):
+        b, c, h, w = x.shape
+        x = x.reshape(b, groups, -1, h, w)
+        x = x.permute(0, 2, 1, 3, 4)
+
+        # flatten
+        x = x.reshape(b, -1, h, w)
+
+        return x
 
     def forward(self, x):
-        return self.act(self.bn(self.conv(x)))
+        b, c, h, w = x.size()
+        #group into subfeatures
+        x=x.view(b*self.G,-1,h,w) #bs*G,c//G,h,w
 
-    def forward_fuse(self, x):
-        return self.act(self.conv(x))
+        #channel_split
+        x_0,x_1=x.chunk(2,dim=1) #bs*G,c//(2*G),h,w
 
-class GSConv(nn.Module):
-    """
-        GSConv is used to merge the channel information of DSConv and BaseConv
-        You can refer to https://github.com/AlanLi1997/slim-neck-by-gsconv for more details
-    """
-    def __init__(self, c1, c2, k=1, s=1, g=1, act=True):
-        super().__init__()
-        c_ = c2 // 2
-        self.cv1 = Conv(c1, c_, k, s, None, g, act)
-        self.cv2 = Conv(c_, c_, 5, 1, None, c_, act)
+        #channel attention
+        x_channel=self.avg_pool(x_0) #bs*G,c//(2*G),1,1
+        x_channel=self.cweight*x_channel+self.cbias #bs*G,c//(2*G),1,1
+        x_channel=x_0*self.sigmoid(x_channel)
 
-    def forward(self, x):
-        x1 = self.cv1(x)
-        x2 = torch.cat((x1, self.cv2(x1)), 1)
+        #spatial attention
+        x_spatial=self.gn(x_1) #bs*G,c//(2*G),h,w
+        x_spatial=self.sweight*x_spatial+self.sbias #bs*G,c//(2*G),h,w
+        x_spatial=x_1*self.sigmoid(x_spatial) #bs*G,c//(2*G),h,w
 
-        b, n, h, w = x2.data.size()
-        b_n = b * n // 2
-        y = x2.reshape(b_n, 2, h * w)
-        y = y.permute(1, 0, 2)
-        y = y.reshape(2, -1, n // 2, h, w)
+        # concatenate along channel axis
+        out=torch.cat([x_channel,x_spatial],dim=1)  #bs*G,c//G,h,w
+        out=out.contiguous().view(b,-1,h,w)
 
-        return torch.cat((y[0], y[1]), 1)
-
-
-class GSConvns(GSConv):
-    # GSConv with a normative-shuffle https://github.com/AlanLi1997/slim-neck-by-gsconv
-    def __init__(self, c1, c2, k=1, s=1, g=1, act=True):
-        super().__init__(c1, c2, k=1, s=1, g=1, act=True)
-        c_ = c2 // 2
-        self.shuf = nn.Conv2d(c_ * 2, c2, 1, 1, 0, bias=False)
-
-    def forward(self, x):
-        x1 = self.cv1(x)
-        x2 = torch.cat((x1, self.cv2(x1)), 1)
-        # normative-shuffle, TRT supported
-        return nn.ReLU(self.shuf(x2))
-
-class GSBottleneck(nn.Module):
-    def __init__(self, c1, c2, k=3, s=1):
-        super().__init__()
-        c_ = c2 // 2
-        # for lighting
-        self.conv_lighting = nn.Sequential(
-            GSConv(c1, c_, 1, 1),
-            GSConv(c_, c2, 1, 1, act=False))
-        # for receptive field
-        self.conv = nn.Sequential(
-            GSConv(c1, c_, 3, 1),
-            GSConv(c_, c2, 3, 1, act=False))
-        self.shortcut = Conv(c1, c2, 3, 1, act=False)
-
-    def forward(self, x):
-        return self.conv_lighting(x) + self.shortcut(x)
-
-
-class DWConv(Conv):
-    # Depth-wise convolution class
-    def __init__(self, c1, c2, k=1, s=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
-        super().__init__(c1, c2, k, s, g=math.gcd(c1, c2), act=act)
-
-
-class GSBottleneckC(GSBottleneck):
-    def __init__(self, c1, c2, k=3, s=1):
-        super().__init__(c1, c2, k, s)
-        self.shortcut = DWConv(c1, c2, 3, 1, act=False)
-
-
-class VoVGSCSP(nn.Module):
-    # VoVGSCSP module with GSBottleneck
-    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
-        super().__init__()
-        c_ = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, c_, 1, 1)
-        self.cv2 = Conv(c1, c_, 1, 1)
-        # self.gc1 = GSConv(c_, c_, 1, 1)
-        # self.gc2 = GSConv(c_, c_, 1, 1)
-        self.gsb = GSBottleneck(c_, c_, 1, 1)
-        self.res = Conv(c_, c_, 3, 1, act=False)
-        self.cv3 = Conv(2 * c_, c2, 1)  #
-
-    def forward(self, x):
-        x1 = self.gsb(self.cv1(x))
-        y = self.cv2(x)
-        return self.cv3(torch.cat((y, x1), dim=1))
+        # channel shuffle
+        out = self.channel_shuffle(out, 2)
+        return out
