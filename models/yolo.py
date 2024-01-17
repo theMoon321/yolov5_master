5134b765fd403a273451fd3cae711088fbb769e3
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2024-01-17 15:08:35 +0800

    Update yolo.py
    Signed-off-by: theMoon321 <130368802+theMoon321@users.noreply.github.com>

--- models/yolo.py
+++ models/yolo.py
@@ -324,7 +324,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         if m in {
                 Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
                 BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, nn.ConvTranspose2d, DWConvTranspose2d, C3x,
-                C3HB, C3RFEM, MultiSEAM, SEAM, C3STR, MobileOneBloc}:
+                C3HB, C3RFEM, MultiSEAM, SEAM, C3STR, MobileOneBlock}:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
