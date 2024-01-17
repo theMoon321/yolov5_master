6f84fb0e9951990e5a6130e1882a88114b8849b2
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2024-01-17 15:05:03 +0800

    Update common.py
    Signed-off-by: theMoon321 <130368802+theMoon321@users.noreply.github.com>

--- models/common.py
+++ models/common.py
@@ -1469,72 +1469,72 @@ class HorBlock(nn.Module):
         x = input + self.drop_path(x)
         return x
 
-class HorNet(nn.Module):
+# class HorNet(nn.Module):
 
-    def __init__(self, index, in_chans, depths, dim_base, drop_path_rate=0.,layer_scale_init_value=1e-6, 
-        gnconv=[
-            partial(gnconv, order=2, s=1.0/3.0),
-            partial(gnconv, order=3, s=1.0/3.0),
-            partial(gnconv, order=4, s=1.0/3.0),
-            partial(gnconv, order=5, s=1.0/3.0), # GlobalLocalFilter
-        ],
-    ):
-        super().__init__()
-        dims = [dim_base, dim_base * 2, dim_base * 4, dim_base * 8]
-        self.index = index
-        self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers by iscyy/air
-        stem = nn.Sequential(
-            nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
-            HorLayerNorm(dims[0], eps=1e-6, data_format="channels_first")
-        )
-        self.downsample_layers.append(stem)
-        for i in range(3):
-            downsample_layer = nn.Sequential(
-                    HorLayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
-                    nn.Conv2d(dims[i], dims[i+1], kernel_size=2, stride=2),
-            )
-            self.downsample_layers.append(downsample_layer)
+#     def __init__(self, index, in_chans, depths, dim_base, drop_path_rate=0.,layer_scale_init_value=1e-6, 
+#         gnconv=[
+#             partial(gnconv, order=2, s=1.0/3.0),
+#             partial(gnconv, order=3, s=1.0/3.0),
+#             partial(gnconv, order=4, s=1.0/3.0),
+#             partial(gnconv, order=5, s=1.0/3.0), # GlobalLocalFilter
+#         ],
+#     ):
+#         super().__init__()
+#         dims = [dim_base, dim_base * 2, dim_base * 4, dim_base * 8]
+#         self.index = index
+#         self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers by iscyy/air
+#         stem = nn.Sequential(
+#             nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
+#             HorLayerNorm(dims[0], eps=1e-6, data_format="channels_first")
+#         )
+#         self.downsample_layers.append(stem)
+#         for i in range(3):
+#             downsample_layer = nn.Sequential(
+#                     HorLayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
+#                     nn.Conv2d(dims[i], dims[i+1], kernel_size=2, stride=2),
+#             )
+#             self.downsample_layers.append(downsample_layer)
 
-        self.stages = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks by iscyy/air
-        dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
+#         self.stages = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks by iscyy/air
+#         dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
 
-        if not isinstance(gnconv, list):
-            gnconv = [gnconv, gnconv, gnconv, gnconv]
-        else:
-            gnconv = gnconv
-            assert len(gnconv) == 4
+#         if not isinstance(gnconv, list):
+#             gnconv = [gnconv, gnconv, gnconv, gnconv]
+#         else:
+#             gnconv = gnconv
+#             assert len(gnconv) == 4
 
-        cur = 0
-        for i in range(4):
-            stage = nn.Sequential(
-                *[HorBlock(dim=dims[i], drop_path=dp_rates[cur + j], 
-                layer_scale_init_value=layer_scale_init_value, gnconv=gnconv[i]) for j in range(depths[i])]
-            )
-            self.stages.append(stage)
-            cur += depths[i] # by iscyy/air
+#         cur = 0
+#         for i in range(4):
+#             stage = nn.Sequential(
+#                 *[HorBlock(dim=dims[i], drop_path=dp_rates[cur + j], 
+#                 layer_scale_init_value=layer_scale_init_value, gnconv=gnconv[i]) for j in range(depths[i])]
+#             )
+#             self.stages.append(stage)
+#             cur += depths[i] # by iscyy/air
 
-        self.apply(self._init_weights)
+#         self.apply(self._init_weights)
 
-    def _init_weights(self, m):
-            if isinstance(m, (nn.Conv2d, nn.Linear)):
-                    nn.init.trunc_normal_(m.weight, std=.02)
-                    nn.init.constant_(m.bias, 0)
-    def forward(self, x):
-        x = self.downsample_layers[self.index](x)
-        x = self.stages[self.index](x)
-        return x
+#     def _init_weights(self, m):
+#             if isinstance(m, (nn.Conv2d, nn.Linear)):
+#                     nn.init.trunc_normal_(m.weight, std=.02)
+#                     nn.init.constant_(m.bias, 0)
+#     def forward(self, x):
+#         x = self.downsample_layers[self.index](x)
+#         x = self.stages[self.index](x)
+#         return x
 
-class CNeB(nn.Module):
-    # CSP ConvNextBlock with 3 convolutions by iscyy/yoloair
-    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
-        super().__init__()
-        c_ = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, c_, 1, 1)
-        self.cv2 = Conv(c1, c_, 1, 1)
-        self.cv3 = Conv(2 * c_, c2, 1)
-        self.m = nn.Sequential(*(ConvNextBlock(c_) for _ in range(n)))
-    def forward(self, x):
-        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
+# class CNeB(nn.Module):
+#     # CSP ConvNextBlock with 3 convolutions by iscyy/yoloair
+#     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+#         super().__init__()
+#         c_ = int(c2 * e)  # hidden channels
+#         self.cv1 = Conv(c1, c_, 1, 1)
+#         self.cv2 = Conv(c1, c_, 1, 1)
+#         self.cv3 = Conv(2 * c_, c2, 1)
+#         self.m = nn.Sequential(*(ConvNextBlock(c_) for _ in range(n)))
+#     def forward(self, x):
+#         return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), dim=1))
 
 class SP(nn.Module):
     def __init__(self, k=3, s=1):
