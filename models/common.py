69d12ef083c6a50659e9f573b051f313d44be711
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2024-01-17 14:34:20 +0800

    Update common.py
    Signed-off-by: theMoon321 <130368802+theMoon321@users.noreply.github.com>

--- models/common.py
+++ models/common.py
@@ -43,20 +43,11 @@ from models.Models.yolov4 import SPPCSP, BottleneckCSP2, Mish
 from models.Models.yolov4 import RepVGGBlockv6, SimSPPF, SimConv, RepBlock
 from models.Models.yolor import ReOrg, DWT, DownC, BottleneckCSPF, ImplicitA, ImplicitM
 from models.Models.Attention.ShuffleAttention import ShuffleAttention
-from models.Models.Attention.CrissCrossAttention import CrissCrossAttention
-from models.Models.Attention.SimAM import SimAM
-from models.Models.Attention.GAMAttention import GAMAttention
-from models.Models.Attention.NAMAttention import NAMAttention
-from models.Models.Attention.S2Attention import S2Attention
-from models.Models.Attention.SEAttention import SEAttention
-from models.Models.Attention.SKAttention import SKAttention
-from models.Models.Attention.SOCA import SOCA
-from models.Models.muitlbackbone import C3GC
 from models.Models.slimneck import GSConv, VoVGSCSP
 
 from models.Models.Lightmodel import CBH, ES_Bottleneck, DWConvblock, ADD, RepVGGBlock, LC_Block, \
     Dense, conv_bn_relu_maxpool, Shuffle_Block, stem, MBConvBlock, mobilev3_bneck
-from models.Models.multibackbone import conv_bn_hswish, DropPath, MobileNetV3_InvertedResidual, DepthSepConv, \
+from models.Models.multibackbone import C3GC, conv_bn_hswish, DropPath, MobileNetV3_InvertedResidual, DepthSepConv, \
     ShuffleNetV2_Model, Conv_maxpool, ConvNeXt, RepLKNet_Stem, RepLKNet_stage1, RepLKNet_stage2, \
         RepLKNet_stage3, RepLKNet_stage4, CoT3, RegNet1, RegNet2, RegNet3, Efficient1, Efficient2, Efficient3, \
             MobileNet1,MobileNet2,MobileNet3, C3STR, ConvNextBlock
